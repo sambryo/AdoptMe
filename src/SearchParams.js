67a03fbd0dcd0d73b66c48f0a1bfb78183fdbd73
Samuel Terefe <terefesam@gmail.com>
2019-07-12 11:56:13 -0700

    fix: custom hooks for labels

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,31 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropDown';
 
 const SearchParams = () => {
-  const location = "San Francisco, CA";
+  const [location, updateLocation] = useState("San Francisco")
+  //const [animal, updateAnimal] = useState("");
+  //const [breed, updateBreed] = useState("");
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
         </label>
+
+        <AnimalDropdown />
+
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
