89e7478b78cd1ad5ecc0cd8cf898e3e8bf055ed4
Samuel Terefe <terefesam@gmail.com>
2019-07-11 18:37:20 -0700

    Initial commit

---
+++ src/SearchParams.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+
+
+const SearchParams = () => {
+  const location = "San Francisco, CA";
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input id="location" value={location} placeholder="Location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
